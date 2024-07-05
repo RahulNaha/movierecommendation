c34c79f3cfe24fd3a90bc287a7af03317899957c
RAHUL NAHA <80236149+RahulNaha@users.noreply.github.com>
2024-07-05 10:38:25 +0530

    Update main.py

--- movierecommender/main.py
+++ movierecommender/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+import os
 
 def fetch_poster(movie_id):
     response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=4984bbed650a08e16170393abb1d75af&language=en-US')
@@ -20,9 +21,17 @@ def recommend(movie):
         recommended_movie_posters.append(fetch_poster(movie_id))
     return recommended_movies, recommended_movie_posters
 
-movie_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+# Get the directory of the current script
+current_dir = os.path.dirname(__file__)
+
+# Construct paths to the pickle files
+movie_dict_path = os.path.join(current_dir, 'movie_dict.pkl')
+similarity_path = os.path.join(current_dir, 'similarity.pkl')
+
+# Load the pickle files
+movie_dict = pickle.load(open(movie_dict_path, 'rb'))
 movies = pd.DataFrame(movie_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+similarity = pickle.load(open(similarity_path, 'rb'))
 
 st.title('Movie Recommendation System')
 
